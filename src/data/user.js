355079c80655ed8a2df2098c6e73aa5097d22d08
Maria Koutsoukalis <mkoutsoukalis@outlook.com>
2023-10-02 16:01:38 -0400

    Completed

--- src/data/user.js
+++ src/data/user.js
@@ -1,6 +1,7 @@
-const username = "Liza";
-const city = "New York";
+const username = "Maria";
+const city = "Atlanta";
 
 const image = "https://i.imgur.com/mV8PQxj.gif";
 
 // export all three variables as named exports
+export { username, city, image };
